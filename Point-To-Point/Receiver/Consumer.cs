41c189a2ba3050dd4e00c502c6ce7691c9e4d553
iancooper <ian_hammond_cooper@yahoo.co.uk>
2018-03-25 13:25:14 +0100

    First version, basic point to point channel demo

---
+++ Point-To-Point/Receiver/Consumer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using SimpleMessaging;
+
+namespace Sender
+{
+    class Consumer
+    {
+        static void Main(string[] args)
+        {
+            using (var channel = new PointToPointChannel("hello-p2p"))
+            {
+                var message = channel.Receive();
+                Console.WriteLine("Received message {0}", message);
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Point-To-Point/SimpleMessaging/PointToPointChannel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace SimpleMessaging
+{
+    public class PointToPointChannel : IDisposable
+    {
+        private const string QueueName = "pm-p2p-text";
+        private const string ExchangeName = "practical-messaging";
+        private readonly IConnection _connection;
+        private readonly IModel _channel;
+
+        public PointToPointChannel(string queueName, string hostName = "localhost")
+        {
+            //just use defaults: usr: guest pwd: guest port:5672 virtual host: /
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            factory.AutomaticRecoveryEnabled = true;
+            _connection = factory.CreateConnection();
+            _channel = _connection.CreateModel();
+            _channel.ExchangeDeclare(ExchangeName, ExchangeType.Direct, durable: false);
+            _channel.QueueDeclare(queue: QueueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
+        }
+
+        public void Send(string message)
+        {
+            var body = Encoding.UTF8.GetBytes(message);
+            _channel.BasicPublish(exchange: ExchangeName, routingKey: QueueName, basicProperties: null, body: body);
+        }
+
+        public string Receive()
+        {
+            var result = _channel.BasicGet(QueueName, autoAck: true);
+            if (result != null)
+                return Encoding.UTF8.GetString(result.Body);
+            else
+                return null;
+        }   
+
+        public void Dispose()
+        {
+            ReleaseUnmanagedResources();
+            GC.SuppressFinalize(this);
+        }
+
+        ~PointToPointChannel()
+        {
+            ReleaseUnmanagedResources();
+        }
+
+
+
+        private void ReleaseUnmanagedResources()
+        {
+            _channel.Close();
+            _connection.Close();
+        }
+    }
+}

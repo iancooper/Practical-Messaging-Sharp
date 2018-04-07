0757f5b973f1e25cdc7b0ae7f22c002062c69c4a
iancooper <ian_hammond_cooper@yahoo.co.uk>
2018-04-07 14:06:30 +0100

    Remove dir with spaces in name

---
+++ polling-consumer/Receiver/Consumer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Model;
+using Newtonsoft.Json;
+using SimpleMessaging;
+
+namespace Sender
+{
+    class Consumer
+    {
+        static void Main(string[] args)
+        {
+            /* We want to force an error on a missing property - you probably don't want this approach in production
+             code as it prevents versioning, but it helps demo invalid messages by creating an error*/
+            using (var channel = new DataTypeChannelConsumer<Greeting>(
+                messageBody => JsonConvert.DeserializeObject<Greeting>(messageBody, new JsonSerializerSettings
+                {
+                    MissingMemberHandling = MissingMemberHandling.Error
+                }))
+            )
+                {
+                var greeting = channel.Receive();
+                if (greeting != null)
+                    Console.WriteLine("Received message {0}", greeting.Salutation);
+                else
+                   Console.WriteLine("Did not receive message"); 
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}

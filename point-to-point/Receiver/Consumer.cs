78c5dbea79478897339650b5c4cc3c8ee39fc175
iancooper <ian_hammond_cooper@yahoo.co.uk>
2018-04-16 01:01:07 +0200

    Fix MacOS case issues

---
+++ point-to-point/Receiver/Consumer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using SimpleMessaging;
+
+namespace Sender
+{
+    class Consumer
+    {
+        static void Main(string[] args)
+        {
+            using (var channel = new PointToPointChannel("hello-p2p"))
+            {
+                var message = channel.Receive();
+                if (message != null)
+                    Console.WriteLine("Received message {0}", message);
+                else
+                   Console.WriteLine("Did not receive message"); 
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ point-to-point/Sender/Producer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using SimpleMessaging;
+
+namespace Sender
+{
+    class Producer
+    {
+        static void Main(string[] args)
+        {
+            using (var channel = new PointToPointChannel("hello-p2p"))
+            {
+                string message = "Hello World!";
+                channel.Send(message);
+                Console.WriteLine("Sent message {0}", message);
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}

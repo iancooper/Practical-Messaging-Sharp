b5d7c8ca90fdaff31a3ecf5810002e27525020eb
iancooper <ian_hammond_cooper@yahoo.co.uk>
2018-06-10 00:26:08 +0100

    Add ToDo

--- polling-consumer/SimpleMessaging/PollingConsumer.cs
+++ polling-consumer/SimpleMessaging/PollingConsumer.cs
@@ -19,22 +19,19 @@ namespace SimpleMessaging
         
         public Task Run(CancellationToken ct)
         {
-            var task = Task.Factory.StartNew(() =>
-                {
-                    ct.ThrowIfCancellationRequested();
-                    using (var channel = new DataTypeChannelConsumer<T>(_messageSerializer, _hostName))
-                    {
-                        while (true)
-                        {
-                            var message = channel.Receive();
-                            _messageHandler.Handle(message);
-                            Task.Delay(1000, ct).Wait(ct); //yield
-                            ct.ThrowIfCancellationRequested();
-                        }
-                    }
-                }, ct
-            );
-            return task;
+            /*
+             * TODO:
+             * Create a Task that will
+             *     check for cancellation
+             *     create a data type channel consumer
+             *     while true
+             *         try go get a message
+             *         dispatch that message to a handler
+             *         yield for 1 second
+             *         check for cancellation
+             *     dispose of the channel
+             *  return the task
+             */
         }
     }
 }

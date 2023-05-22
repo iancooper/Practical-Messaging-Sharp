549b7de19632fcc00145eca4e0f9ff3c87f979be
Ian Cooper <ian.cooper@justeattakeaway.com>
2023-05-22 15:06:39 +0200

    Fix stale issues

--- point-to-point/SimpleMessaging/PointToPointChannel.cs
+++ point-to-point/SimpleMessaging/PointToPointChannel.cs
@@ -65,7 +65,7 @@ namespace SimpleMessaging
         {
             var result = _channel.BasicGet(_queueName, autoAck: true);
             if (result != null)
-                return Encoding.UTF8.GetString(result.Body);
+                return Encoding.UTF8.GetString(result.Body.ToArray());
             else
                 return null;
         }   
